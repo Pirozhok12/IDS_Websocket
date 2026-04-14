24ddbd60854f757613fcecb3af0b538c74d4212b
Pirozhok12 <mishabozkho@gmail.com>
2026-04-15 01:13:25 +0300

    redy for WEBsocket

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express"); // окрема змінна для зручності
+var path = require('path'); 
+
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use(express.static('public')); // вказівка каталогу для статичних ресурсів, у якому буде розташовано файл css, що підключається.
+
+app.get('/', function(req, res) { 
+  res.sendFile(__dirname + '/public/html/menu.html'); 
+
+});
+
+io.on('connection', function(socket){
+  socket.on('send message', function(msg) {
+	io.emit('receive message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
